f604639f9f7794f75bc81922aa23b57658d8221f
Jorge Del Castillo <jorge.ale91@gmail.com>
2019-08-11 19:44:50 +0200

    JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const jsx = <h1>Hello, platzi Badges!</h1>;
+// const element = React.createElement(
+//   "a",
+//   {
+//     href: "https://platzi.com"
+//   },
+//   "Ir a platzi"
+// );
+const name = "Zryru";
+// const element = React.createElement(
+//     "h1",
+//     {},
+//     `Hola soy, ${name}`
+// )
+const jsx = <h1> Hola, soy {name}</h1>
+const container = document.getElementById("root");
+
+ReactDOM.render(jsx, container);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
