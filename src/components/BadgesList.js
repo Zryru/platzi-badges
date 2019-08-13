5c717764f00760a4bb3a2c00362a4cd6d8ff4196
Jorge Del Castillo <jorge.ale91@gmail.com>
2019-08-13 22:29:22 +0200

    random style

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import "../styles/Badge.css";
+import logo from "../assets/images/splat4.jpg";
+import avatar from "../assets/images/splat2.jpg";
 
 export default class BadgesList extends Component {
   render() {
@@ -6,8 +9,16 @@ export default class BadgesList extends Component {
       <ul className="list-unstyled">
         {this.props.badges.map(badge => {
           return (
-            <li key={badge.id}>
-              <p>{badge.name}</p>
+            <li class="Badge-list__container" key={badge.id}>
+              <div className="row">
+                <div className="col-3">
+                  <img className="Badge__avatar avatar" src={avatar} alt="avatar" />
+                </div>
+                <div className="col-9">
+                  <p>{badge.name}</p>
+                  <p>{badge.twitter}</p>
+                </div>
+              </div>
             </li>
           );
         })}
